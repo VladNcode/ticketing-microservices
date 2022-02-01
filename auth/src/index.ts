df8c6305dbc242f8c98b6766aede4f0f39d5073e
unknown <vladncode@gmail.com>
2022-02-01 15:36:50 +0200

    error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,26 @@
 import express from 'express';
+import { currentUserRouter } from './routes/current-user';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+import { signinRouter } from './routes/signin';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 
+//* Json and query
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
+//* Routing
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+
+//* Error handler
+app.use(errorHandler);
+
+//* Starting server
 app.listen(3000, () => {
   console.log('Listening on 3000');
 });
