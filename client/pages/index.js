270ccde589a83e8ddb6868e3da790f8e404b41e9
vladncode <vladncode@gmail.com>
2022-02-04 15:04:07 +0200

    refactored initial props

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,27 +1,23 @@
 import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
-  // console.log(currentUser);
-  // axios.get('/api/users/currentuser').catch(err => {
-  //   console.log(err.message);
-  // });
-
   console.log(currentUser);
-
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async function () {
+export async function getServerSideProps({ req }) {
   const response = await axios.get(
     'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
     {
-      headers: {
-        Host: 'ticketing.dev',
-      },
+      headers: req.headers,
     }
   );
 
-  return response.data;
-};
+  return {
+    props: {
+      currentUser: response.data.currentUser,
+    },
+  };
+}
 
 export default LandingPage;
