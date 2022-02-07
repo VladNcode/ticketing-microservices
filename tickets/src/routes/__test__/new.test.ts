556de70bcbd0eb50f8ca1c08c48119b810c1c5a6
vladncode <vladncode@gmail.com>
2022-02-07 09:08:07 +0200

    tests for tickets

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a routehandler listening to /api/tickets for post requests', async () => {
   const res = await request(app).post('/api/tickets').send({});
@@ -16,6 +17,59 @@ it('returns status other than 401 if user is signed in', async () => {
   expect(res.status).not.toEqual(401);
 });
 
-it('returns an error if an invalid title is provided', async () => {});
-it('returns an error if an invalid price is provided', async () => {});
-it('creates a ticket with valid inputs', async () => {});
+it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signup())
+    .send({
+      title: '',
+      price: 10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signup())
+    .send({
+      price: 10,
+    })
+    .expect(400);
+});
+
+it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signup())
+    .send({
+      title: 'title',
+      price: -10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signup())
+    .send({
+      title: 'title',
+    })
+    .expect(400);
+});
+
+it('creates a ticket with valid inputs', async () => {
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signup())
+    .send({
+      title: 'title',
+      price: 10,
+    })
+    .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].title).toEqual('title');
+  expect(tickets[0].price).toEqual(10);
+});
