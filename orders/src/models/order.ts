5bdc6f15bed7f4e7bde766cfeb867cc3f6763957
VladNCode <vldsmrfow@gmail.com>
2022-02-09 17:17:36 +0200

    orders routes and tests

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -28,18 +28,15 @@ const orderSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-
     status: {
       type: String,
       required: true,
       enum: Object.values(OrderStatus),
       default: OrderStatus.Created,
     },
-
     expiresAt: {
       type: mongoose.Schema.Types.Date,
     },
-
     ticket: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Ticket',
--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,33 @@
 import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import {
+  NotFoundError,
+  requireAuth,
+  NotAuthError,
+  BadRequestError,
+} from '@vnctickets/common/build';
+import { Order, OrderStatus } from '../models/order';
 
 const router = express.Router();
 
-router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
+router.get('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
+  const validId = mongoose.Types.ObjectId.isValid(req.params.orderId);
+
+  if (!validId) {
+    throw new BadRequestError('Invalid order id!');
+  }
+
+  const order = await Order.findById(req.params.orderId).populate('ticket');
+
+  if (!order) {
+    throw new NotFoundError();
+  }
+
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthError();
+  }
+
+  res.send(order);
 });
 
 export { router as showOrderRouter };
