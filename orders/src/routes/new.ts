7f4a4001e3c9253fe4a5716b9597865f8f410fc1
VladNCode <vldsmrfow@gmail.com>
2022-02-09 11:49:17 +0200

    new order tests

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,20 @@
 import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@vnctickets/common/build';
 import { body } from 'express-validator';
+
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  BadRequestError,
+  OrderStatus,
+} from '@vnctickets/common/build';
+
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
+
+// 15 minutes
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 const router = express.Router();
 
@@ -17,7 +30,38 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    res.send({});
+    //* Find the ticket the user is trying to order in the database
+    const { ticketId } = req.body;
+
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    //* Make sure that this ticket is not already reserved
+    const isReserved = await ticket.isReserved();
+
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved!');
+    }
+
+    //* Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    //* Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+
+    await order.save();
+
+    //* Publish an event saying the order has been created
+
+    res.status(201).send(order);
   }
 );
 
---
+++ orders/src/test/setup.ts
@@ -0,0 +1,53 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+declare global {
+  var signup: () => string[];
+}
+
+jest.mock('../nats-wrapper');
+
+let mongo: any;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'string';
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signup = () => {
+  // Build a JWT payload: {id, email}
+  const payload = { id: new mongoose.Types.ObjectId().toHexString(), email: 'test@test.com' };
+
+  // Create JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // Build session object: {jwt: myjwt_token}
+  const session = { jwt: token };
+
+  // Turn session into JSON
+  const sessionJson = JSON.stringify(session);
+
+  // Encode JSON as base64
+  const base64 = Buffer.from(sessionJson).toString('base64');
+
+  // Return a string thats a cookie with an encoded data
+  return [`session=${base64}`];
+};
