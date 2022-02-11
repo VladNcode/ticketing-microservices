585787a8db1a3dd417d3d5b22ece6722f5bca54b
VladNCode <vldsmrfow@gmail.com>
2022-02-11 12:28:27 +0200

    started to build payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@vnctickets/common';
+
+const app = express();
+app.set('trust proxy', true);
+
+//* Json and query
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+//* Cookie-session
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV === 'test' ? false : true,
+  })
+);
+
+//* Middlewares
+app.use(currentUser);
+
+//* Routing
+
+app.all('*', (req, res, next) => {
+  throw new NotFoundError();
+});
+
+//* Error handler
+app.use(errorHandler);
+
+export { app };
